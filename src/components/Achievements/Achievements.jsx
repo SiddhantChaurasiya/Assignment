ea62a41d439e5501570a417a6e3b3215a9453993
Siddhant Chaurasiya <siddhantchaurasiya28@gmail.com>
2025-10-06 10:53:47 +0530

    add logo images

--- src/components/Achievements/Achievements.jsx
+++ src/components/Achievements/Achievements.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import './Achievements.css'
+import awardLogo from '/src/assets/awards/logo.png';
+import awardsBanner from '/src/assets/awards/awards-banner.png';
 
 const Achievements = () => {
   return (
     <section className="achievements-section">
       <h2 className="achievements-title">Our Achievements</h2>
       <div className="logos-container">
-        <img src='/src/assets/awards/logo.png' alt="REIWA Awards Logo" className="award-logo-single" />
-        <img src='/src/assets/awards/awards-banner.png' alt="Row of REIWA Awards" className="award-logo-banner" />
+        <img src={awardLogo} alt="REIWA Awards Logo" className="award-logo-single" />
+        <img src={awardsBanner} alt="Row of REIWA Awards" className="award-logo-banner" />
       </div>
     </section>
   )
